0315489b9cb418782548e15246055dbb05379ae7
enryleinhard <42536568+enryleinhard@users.noreply.github.com>
2023-11-09 00:22:06 +0800

    feat: add template commands

--- client.py
+++ client.py
@@ -1,27 +1,41 @@
 import socket
 
+CLIENT_REPL_PREFIX_STR = 'client: '
+BUFFER_SIZE = 4096
+
 class ClientSocket:
   def __init__(self, host, port):
     self.host = host
     self.port = port
     self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     self.socket.connect((self.host, self.port))
-
-  def send(self, msg):
-    self.socket.send(bytes(msg, encoding='utf-8'))
+  
+  def send(self, message):
+    self.socket.send(bytes(message, encoding='utf-8'))
 
   def recv(self):
-    return self.socket.recv(4096)
+    return self.socket.recv(BUFFER_SIZE)
   
   def close(self):
     self.socket.close()
 
 if __name__ == '__main__':
-  client = ClientSocket('localhost', 16011)
+  client_socket = ClientSocket('172.27.160.121', 16011)
+
   while True:
-    msg = input('')
-    client.send(msg)
-    print(client.recv().decode('utf-8'))
-    if msg == 'EXIT':
-      client.close()
-      break
+    command = input(CLIENT_REPL_PREFIX_STR)
+    
+    if command == 'POST_STRING':
+      #TODO: implement
+      continue
+    elif command == 'POST_FILE':
+      #TODO: implement
+      continue
+    elif command == 'GET':
+      #TODO: implement
+      continue
+    elif command == 'EXIT':
+      client_socket.close()
+    else:
+      client_socket.send(command)
+      print(client_socket.recv().decode('utf-8'))
