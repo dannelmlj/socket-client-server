f7299f32498d3a24ccd2a82ee499d62625b8c821
enryleinhard <42536568+enryleinhard@users.noreply.github.com>
2023-11-10 20:42:41 +0800

    feat: bulletin board commands

--- client.py
+++ client.py
@@ -12,30 +12,39 @@ CLIENT_INPUT_COMMAND_PREFIX_STR     = "Input command: "
 CLIENT_INPUT_REPL_PREFIX_STR        = "client: "
 
 # Header Strings
-INITIALIZE_SOCKET_HEADER_STR        = "----------------------- Initialize Socket -----------------------"
-INPUT_COMMAND_HEADER_STR            = "------------------------- Input Command -------------------------"
-NEXT_COMMAND_HEADER_STR             = "------------------------- Next Command --------------------------"
-POST_STRING_COMMAND_HEADER_STR      = "--------- Post String Command (Type a single '&' to end) --------"
-GET_COMMAND_HEADER_STR              = "-------------------------- Get Command --------------------------"
-
-# Message Strings
-CONNECT_STATUS_STR                  = "Connect status: "
-SEND_STATUS_STR                     = "Send status: "
-RECEIVED_MESSAGE_STR                = "---Received messages---"
-POST_FILE_ERROR_FILE_NOT_FOUND_MSG  = "Error: File not found"
-CONNECTION_FAIL_NOT_BUILT_MSG       = "Error: Connection is not built, please try again"
+HEADER_INITIALIZE_SOCKET_STR        = "----------------------- Initialize Socket -----------------------"
+HEADER_INPUT_COMMAND_STR            = "------------------------- Input Command -------------------------"
+HEADER_NEXT_COMMAND_STR             = "------------------------- Next Command --------------------------"
+HEADER_POST_STRING_COMMAND_STR      = "--------- Post String Command (Type a single '&' to end) --------"
+HEADER_GET_COMMAND_STR              = "-------------------------- Get Command --------------------------"
 
 # Utility Strings
 DIVIDER_STR                         = "------"
+CONNECT_STATUS_STR                  = "Connect status: "
+SEND_STATUS_STR                     = "Send status: "
+RECEIVED_MESSAGE_STR                = "---Received messages---"
+
+# Error Strings
+ERROR_POST_FILE_FILE_NOT_FOUND_STR  = "Error: File not found"
+ERROR_POST_FILE_SIZE_EXCEED_STR     = "Error: File size exceeds the maximum file size"
+ERROR_CONNECTION_FAIL_STR           = "Error: Connection is not built, please try again"
+
 
 # Size Constants
-MAXIMUM_FILE_SIZE_IN_BYTES = 256
-BUFFER_SIZE = 4096
+MAXIMUM_FILE_SIZE_IN_BYTES          = 256
+BUFFER_SIZE                         = 4096
+
+# Command Constants
+POST_STRING_COMMAND                 = 'POST_STRING'
+POST_FILE_COMMAND                   = 'POST_FILE'
+GET_COMMAND                         = 'GET'
+EXIT_COMMAND                        = 'EXIT'
 
 class BulletinBoardCommands:
+
   @staticmethod
   def post_string(bulletin_board_client):
-    ''' Post_string command.
+    ''' POST_STRING command.
         This command allows client to send a text file to the server line by line. 
         The client must type the text (using the standard input) line-by-line. 
         The first line is the command itself (i.e. POST_STRING) and subsequent lines are treated as the text. 
@@ -44,24 +53,54 @@ class BulletinBoardCommands:
         Keyword arguments:
         bulletin_board_client -- BulletinBoardClient object to send the message to the server through the socket
     '''
-    bulletin_board_client.send('POST_STRING')
-    print(POST_STRING_COMMAND_HEADER_STR)
+    bulletin_board_client.send(POST_STRING_COMMAND)
+    print(HEADER_POST_STRING_COMMAND_STR)
     post_string_msg = ''
-    message_count = 0;
+    message_count = 0
     while (post_string_msg != '&'):
       post_string_msg = input(CLIENT_INPUT_REPL_PREFIX_STR)
       bulletin_board_client.send(post_string_msg)
       message_count += 1
       if (post_string_msg == '&'):
-        print(bulletin_board_client.recv().decode('utf-8'))
+        print(bulletin_board_client.recv())
         print(DIVIDER_STR)
         print(f'Sent {message_count} messages to (IP Address: {bulletin_board_client.host}, Port Number: {bulletin_board_client.port})')
         print(DIVIDER_STR)
-    # firstCommand = False
 
   @staticmethod
   def post_file(bulletin_board_client):
-    bulletin_board_client.send('POST_FILE')
+    ''' POST_FILE command.
+        This command allows client to send a text file to the server.
+        The client will supply the file path and the server will read the file and store it in the server side.
+
+        Keyword arguments:
+        bulletin_board_client -- BulletinBoardClient object to send the message to the server through the socket
+    '''
+    bulletin_board_client.send(POST_FILE_COMMAND)
+    print(bulletin_board_client.recv())
+
+    file_path = input(CLIENT_INPUT_REPL_PREFIX_STR)
+    file_size = os.stat(file_path).st_size
+    file_header = struct.pack('128sl', bytes(file_path, encoding='utf-8'), file_size)
+
+    if not os.path.isfile(file_path):
+      print(ERROR_POST_FILE_FILE_NOT_FOUND_STR)
+      bulletin_board_client.send('close')
+      print(bulletin_board_client.recv())
+      return
+    
+    if file_size > MAXIMUM_FILE_SIZE_IN_BYTES:
+      print(ERROR_POST_FILE_SIZE_EXCEED_STR)
+      bulletin_board_client.send('close')
+      bulletin_board_client.recv()
+      return
+    
+    bulletin_board_client.socket.send(file_header)
+    with open(file_path, 'rb') as file:
+      file_data = file.read(MAXIMUM_FILE_SIZE_IN_BYTES)
+      bulletin_board_client.socket.send(file_data)
+
+    print(bulletin_board_client.recv())
 
   @staticmethod
   def get(bulletin_board_client):
@@ -72,18 +111,17 @@ class BulletinBoardCommands:
         Keyword arguments:
         bulletin_board_client -- BulletinBoardClient object to receive the message from the server through the socket
     '''
-    bulletin_board_client.send('GET')
-    print(GET_COMMAND_HEADER_STR)
+    bulletin_board_client.send(GET_COMMAND)
+    print(HEADER_GET_COMMAND_STR)
     print(RECEIVED_MESSAGE_STR)
     get_string_msg = ''
     while (get_string_msg != 'server: &'):
-      get_string_msg = bulletin_board_client.recv().decode('utf-8')
+      get_string_msg = bulletin_board_client.recv()
       print(get_string_msg)
       if (get_string_msg == 'server: &'):
-        print (DIVIDER_STR)
-        print (f'IP Address: {bulletin_board_client.host}, Port Number: {bulletin_board_client.port}')
-        print (DIVIDER_STR)
-    # firstCommand = False
+        print(DIVIDER_STR)
+        print(f'IP Address: {bulletin_board_client.host}, Port Number: {bulletin_board_client.port}')
+        print(DIVIDER_STR)
 
   @staticmethod
   def exit(bulletin_board_client):
@@ -94,9 +132,21 @@ class BulletinBoardCommands:
         Keyword arguments:
         bulletin_board_client -- BulletinBoardClient object to send the exit command and receive the response from the server through the socket
     '''
-    bulletin_board_client.send('EXIT')
-    print(client_socket.recv().decode('utf-8'))
+    bulletin_board_client.send(EXIT_COMMAND)
+    print(bulletin_board_client.recv())
     bulletin_board_client.close()
+
+  @staticmethod
+  def unknown(bulletin_board_client, command):
+    ''' UNKNOWN command.
+        This is a utility function to handle unknown commands.
+
+        Keyword arguments:
+        bulletin_board_client -- BulletinBoardClient object to send the unknown command and receive the response from the server through the socket
+        command -- the unknown command
+    '''
+    bulletin_board_client.send(command)
+    print(bulletin_board_client.recv())
 
 class BulletinBoardClient:
   def __init__(self, host, port):
@@ -108,66 +158,51 @@ class BulletinBoardClient:
     self.socket.connect((self.host, self.port))
 
   def send(self, message):
-    self.socket.send(bytes(message, encoding='utf-8'))
+    self.socket.sendall(message.encode())
 
   def recv(self):
-    return self.socket.recv(BUFFER_SIZE).decode('utf-8')
-
-  def z_recv(self):
-    return self.socket.recv(BUFFER_SIZE)
+    return self.socket.recv(BUFFER_SIZE).decode()
   
   def close(self):
     self.socket.close()
 
-if __name__ == '__main__':
-  print(INITIALIZE_SOCKET_HEADER_STR)
+def main():
+  print(HEADER_INITIALIZE_SOCKET_STR)
+
   ip_address = input(CLIENT_INPUT_IP_ADDRESS_PREFIX_STR)
   port_number = int(input(CLIENT_INPUT_PORT_NUMBER_PREFIX_STR))
-  client_socket = BulletinBoardClient(ip_address, port_number)
 
-  client_socket.initialize()
-
-  firstCommand = True
-  while True:
-    if firstCommand:
-      print(INPUT_COMMAND_HEADER_STR)
-    else:
-      print(NEXT_COMMAND_HEADER_STR)
-    command = input(CLIENT_INPUT_COMMAND_PREFIX_STR)
-  
-
-    if command == 'POST_FILE':
-      client_socket.send(command)
-      print(client_socket.z_recv().decode('utf-8'))
-      file_path = input(CLIENT_INPUT_REPL_PREFIX_STR)
-
-      if not os.path.isfile(file_path):
-        print('Error: File not found')
-        client_socket.send('close')
-        print(client_socket.z_recv().decode('utf-8'))
-        firstCommand = False
-        continue
-        
-      if os.stat(file_path).st_size > MAXIMUM_FILE_SIZE_IN_BYTES:
-        print('Error: File size is too large')
-        client_socket.send('close')
-        print(client_socket.z_recv().decode('utf-8'))
-        firstCommand = False
-        continue
-
-      file_header = struct.pack('128sl', bytes(file_path, encoding='utf-8'), os.stat(file_path).st_size)
-      client_socket.socket.send(file_header)
-      file = open(file_path, 'rb')
-      file_data = file.read(MAXIMUM_FILE_SIZE_IN_BYTES)
-      file.close()
-      client_socket.socket.send(file_data)
-
-      print(client_socket.z_recv().decode('utf-8'))
-      firstCommand = False
-      continue
-
-
-      
+  connection_established = False
+  while not connection_established:
+    try:
+      client_socket = BulletinBoardClient(ip_address, port_number)
+      client_socket.initialize()
+      connection_established = True
+    except (ConnectionError, ValueError, OverflowError) as e:
+      print(ERROR_CONNECTION_FAIL_STR)
     
-    client_socket.send(command)
-    print(client_socket.z_recv().decode('utf-8'))
+  sent_commands = 0
+  while True:
+
+    if not sent_commands:
+      print(HEADER_INPUT_COMMAND_STR)
+    else:
+      print(HEADER_NEXT_COMMAND_STR)
+
+    command = input(CLIENT_INPUT_COMMAND_PREFIX_STR)
+    sent_commands += 1
+
+    if command == POST_STRING_COMMAND:
+      BulletinBoardCommands.post_string(client_socket)
+    elif command == POST_FILE_COMMAND:
+      BulletinBoardCommands.post_file(client_socket)
+    elif command == GET_COMMAND:
+      BulletinBoardCommands.get(client_socket)
+    elif command == EXIT_COMMAND:
+      BulletinBoardCommands.exit(client_socket)
+      break
+    else:
+      BulletinBoardCommands.unknown(client_socket, command)
+
+if __name__ == '__main__':
+  main()
