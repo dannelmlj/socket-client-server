11411901777d863e53bd7a85c0955b623bf1410e
Dannel <101443060+dannelmlj@users.noreply.github.com>
2023-11-10 17:32:13 +0800

    refactor: move constants to constant file (#4)

---
+++ constants.py
@@ -0,0 +1,27 @@
+# Input Prefix Strings
+CLIENT_INPUT_IP_ADDRESS_PREFIX_STR  = "Input IP Address: "
+CLIENT_INPUT_PORT_NUMBER_PREFIX_STR = "Input port number: "
+CLIENT_INPUT_COMMAND_PREFIX_STR     = "Input command: "
+CLIENT_INPUT_REPL_PREFIX_STR        = "client: "
+
+# Header Strings
+INITIALIZE_SOCKET_HEADER_STR        = "----------------------- Initialize Socket -----------------------"
+INPUT_COMMAND_HEADER_STR            = "------------------------- Input Command -------------------------"
+NEXT_COMMAND_HEADER_STR             = "------------------------- Next Command --------------------------"
+POST_STRING_COMMAND_HEADER_STR      = "--------- Post String Command (Type a single '&' to end) --------"
+GET_COMMAND_HEADER_STR              = "-------------------------- Get Command --------------------------"
+
+# Message Strings
+CONNECT_STATUS_STR                  = "Connect status: "
+SEND_STATUS_STR                     = "Send status: "
+RECEIVED_MESSAGE_STR                = "---Received messages---"
+POST_FILE_ERROR_FILE_NOT_FOUND_MSG  = "Error: File not found"
+CONNECTION_FAIL_NOT_BUILT_MSG       = "Error: Connection is not built, please try again"
+
+
+# Utility Strings
+DIVIDER_STR                         = "------"
+
+# Size Constants
+MAXIMUM_FILE_SIZE_IN_BYTES = 256
+BUFFER_SIZE = 4096
