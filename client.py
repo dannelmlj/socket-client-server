b835edec1525eff00dd0e585fb6415c570c34c4f
dannelmlj <dannelmuljaa@gmail.com>
2023-11-10 17:23:49 +0800

    refactor: move constants to constant file

--- client.py
+++ client.py
@@ -1,33 +1,7 @@
 import os
 import socket
 import struct
-
-# Input Prefix Strings
-CLIENT_INPUT_IP_ADDRESS_PREFIX_STR = 'Input IP Address: '
-CLIENT_INPUT_PORT_NUMBER_PREFIX_STR = 'Input port number: '
-CLIENT_INPUT_COMMAND_PREFIX_STR = 'Input command: '
-CLIENT_INPUT_REPL_PREFIX_STR = 'client: '
-
-# Header Strings
-INITIALIZE_SOCKET_HEADER_STR = '---------- Initialize Socket ----------'
-INPUT_COMMAND_HEADER_STR = '---------- Input Command ----------'
-NEXT_COMMAND_HEADER_STR = '---------- Next Command ----------'
-POST_STRING_COMMAND_HEADER_STR = "---------- Post String Command (Type a single '&' to end) ----------"
-GET_COMMAND_HEADER_STR = "---------------- Get Command ---------------"
-
-# Utility Strings
-ERROR_STR = 'Error: '
-DIVIDER_STR = '------'
-CONNECT_STATUS_STR = 'Connect status: '
-SEND_STATUS_STR = 'Send status: '
-
-RECEIVED_MESSAGE_STR = '---Received message--- '
-
-# Message Strings
-POST_FILE_ERROR_FILE_NOT_FOUND_MSG = 'Error: File not found'
-
-MAXIMUM_FILE_SIZE_IN_BYTES = 256
-BUFFER_SIZE = 4096
+from constants import *
 
 class ClientSocket:
   def __init__(self, host, port):
